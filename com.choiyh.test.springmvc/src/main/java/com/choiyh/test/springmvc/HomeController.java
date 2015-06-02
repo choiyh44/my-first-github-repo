d8383674a625fd413e0f9b689432e731d0a13550
choiyh44 <choiyh44@gmail.com>
2015-06-02 10:16:48 +0900

    first commit

---
+++ com.choiyh.test.springmvc/src/main/java/com/choiyh/test/springmvc/HomeController.java
@@ -0,0 +1,46 @@
+package com.choiyh.test.springmvc;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@Autowired
+	Environment env;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		logger.debug("test.str: " + env.getProperty("test.str"));
+		
+		return "home";
+	}
+	
+}
---
+++ com.choiyh.test.springmvc/src/main/java/com/choiyh/test/springmvc/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.choiyh.test.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+ 
+@Configuration
+@PropertySource("classpath:/config/application.properties")
+public class AppConfig {
+ 
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+ 
+}
